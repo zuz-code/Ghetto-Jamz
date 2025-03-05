af6b4ed36ae9de3a33fae4cafaf21b7ba788e3f8
zuz-code <robertzuze55@gmail.com>
2025-03-05 08:47:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
             e.innerHTML = "Menu";
             menuList.style.display = "none";
         }
-    }
+    };
 
 const button =
 document.getElementById('download-btn');
@@ -24,17 +24,14 @@ button.addEventListener('click', (clickCount) =>
 
 
 
-
-
-
 const search = ()=>{
     const searchbox = document.getElementById("searchitem").value.toUpperCase();
     const storeitems = document.getElementById("music-cont")
-    const product = querySelectorAll("p")
+    const product = querySelectorAll("a")
     const pname = storeitems.getElementsByTagName("#musicitem")
     
     for(var i=0; i < pname.length; i++){
-        let match = product[i].getElementsByTagName('p')[0];
+        let match = product[i].getElementsByTagName('a')[0];
 
         if(match){
             let textvalue = match.textContent || match.innerHTML
@@ -50,10 +47,51 @@ const search = ()=>{
 
      
 
+function tikakamizeClickCount(){
+    if(localStorage.clickCount)
+    {
+        localStorage.clickCount=parseFloat(localStorage.clickCount)+1;
+    }
+    else
+    {
+        localStorage.clickCount=1;
+    }
+    var a=document.getElementById("tikakamize-clickcount");
+    a.value=localStorage.clickCount;
+
+}
 
 
 
 
 
+function khomfiClickCount(){
+    if(localStorage.clickCount)
+    {
+        localStorage.clickCount=parseInt(localStorage.clickCount)+1;
+    }
+    else
+    {
+        localStorage.clickCount=1;
+    }
+    var a=document.getElementById("khomfi-clickcount");
+    a.value=localStorage.clickCount;
 
-   
+}
+
+
+
+
+function chumaClickCount(){
+    if(localStorage.clickCount)
+    {
+        localStorage.clickCount=parseInt(localStorage.clickCount)+1;
+    }
+    else
+    {
+        localStorage.clickCount=1;
+    }
+    var a=document.getElementById("chuma-clickcount");
+    a.value=localStorage.clickCount;
+
+}
